1c79f00d5d2dc7bdf5dccac439e5c849965f7f78
moeen775 <moeenahmmed2015br@gmail.com>
2023-08-25 00:04:22 +0600

    session 1 for JPA

--- src/main/java/com/spring/jpa/Application.java
+++ src/main/java/com/spring/jpa/Application.java
@@ -1,13 +1,60 @@
 package com.spring.jpa;
 
+import com.spring.jpa.entity.Student;
+import com.spring.jpa.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student moeen = new Student(
+                    "Moeen",
+                    "Ahmmed",
+                    "moeen@gmail.com",
+                    25);
+
+            Student maria = new Student(
+                    "Maria",
+                    "Ahmmed",
+                    "maria@gmail.com",
+                    25);
+            System.out.println("Saving Student list");
+            studentRepository.saveAll(List.of(moeen, maria));
+
+            System.out.println("Counting Number of Student in database");
+            System.out.println(studentRepository.count());
+
+            studentRepository
+                    .findById(2L)
+                    .ifPresentOrElse(System.out::println,
+                            () -> System.out.println("Student is not present with ID 2"));
+
+            studentRepository
+                    .findById(3L)
+                    .ifPresentOrElse(System.out::println,
+                            () -> System.out.println("Student is not present with ID 3"));
+
+            List<Student> studentList = studentRepository.findAll();
+            studentList.forEach(System.out::println);
+
+            studentRepository.deleteById(1L);
+
+            System.out.println(studentRepository.count());
+
+            studentRepository.findStudentByEmail("moeen@gmail.com").ifPresentOrElse(System.out::println,
+                    () -> System.out.println("Student is not present with ID 3"));
+        };
+    }
 }
---
+++ src/main/java/com/spring/jpa/entity/Student.java
@@ -0,0 +1,91 @@
+package com.spring.jpa.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+import static jakarta.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "student", uniqueConstraints = {
+        @UniqueConstraint(name = "student_email_unique",
+                columnNames = "email")})
+public class Student implements Serializable {
+    @Id
+    @SequenceGenerator(name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE,
+            generator = "student_sequence")
+    @Column(name = "id",
+            updatable = false)
+    private Long id;
+    @Column(name = "first_name",
+            updatable = false,
+            columnDefinition = "TEXT")
+    private String firstName;
+
+    @Column(name = "last_name",
+            updatable = false,
+            columnDefinition = "TEXT")
+    private String LastName;
+
+    @Column(name = "email",
+            updatable = false,
+            columnDefinition = "TEXT")
+    private String email;
+
+    @Column(name = "age",
+            nullable = false)
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student( String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        LastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
